2764cc3d89db6958badb68fcf98d8e40d172238e
Andrea Marucci <marucci.esports@gmail.com>
2022-08-12 12:46:35 +0200

    Update executeButton.js

--- handlers/executeButton.js
+++ handlers/executeButton.js
@@ -18,7 +18,7 @@ const executeButton = async (interaction) => {
       .then((result) => {
         const embedMessage = require("../discord/embedMessage");
         const role = interaction.channel.guild.roles.cache.get(result[0].role);
-        if (interaction.member.roles.has(role)) {
+        if (interaction.member.cache.roles.has(role)) {
           // interaction.member.roles.remove(role);
           // embedMessage.reply(
           //   interaction,
